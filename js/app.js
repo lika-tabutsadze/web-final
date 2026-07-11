da0a18e895cff1bcaa0e69a0b73757230d6b36b2
lika-tabutsadze <lika.tabutsadze1@gmail.com>
2026-07-11 18:11:24 +0400

    finished the about us page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 // BURGER MENU
 
+
 function BurgerMenu() {
     const toggle = document.getElementById('burger__menu');
     const navbar = document.getElementById('navbar');
     
-    
-    toggle.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
-    
+    if (toggle && navbar) {
+        toggle.addEventListener('click', () => {
+            navbar.classList.toggle('active');
+        });
+    }
 }
 
 BurgerMenu()
